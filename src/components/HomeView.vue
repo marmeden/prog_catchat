7ccd0d6ac4387413f56e86bc5602190895fb3181
marmeden <itsme@eneasmarin.es>
2018-09-25 17:40:21 +0200

    Firebase conn

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -3,33 +3,31 @@
     <div class="mdl-grid">
       <div class="mdl-cell mdl-cell--3-col mdl-cell mdl-cell--1-col-tablet mdl-cell--hide-phone"></div>
       <div class="mdl-cell mdl-cell--6-col mdl-cell--4-col-phone">
-        <div v-for="picture in this.pictures" class="image-card" @click="displayDetails(picture.id)">
-          <div class="image-card__picture">
-            <img :src="picture.url" />
-          </div>
-          <div class="image-card__comment mdl-card__actions">
-            <span>{{ picture.comment }}</span>
-          </div>
+      <div v-for="picture in this.$root.cat" class="image-card" @click="displayDetails(picture['.key'])">
+        <div class="image-card__picture">
+          <img :src="picture.url" />
         </div>
+        <div class="image-card__comment mdl-card__actions">
+          <span>{{ picture.comment }}</span>
+        </div>
+      </div>
       </div>
     </div>
     <router-link class="add-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored" to="/post">
       <i class="material-icons">add</i>
     </router-link>
+    <router-link class="take-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored" to="/camera">
+      <i class="material-icons">camera_alt</i>
+    </router-link>
   </div>
 </template>
 
+
 <script>
-  import data from '../data'
   export default {
     methods: {
       displayDetails (id) {
         this.$router.push({name: 'detail', params: { id: id }})
-      }
-    },
-    data () {
-      return {
-        'pictures': data.pictures
       }
     }
   }
